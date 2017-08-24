24b2701384dcb9662198a929049f348c212ba270
surajv5 <30804526+surajv5@users.noreply.github.com>
2017-08-24 19:30:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
 console.log('Loaded!');
+var element=document.getElementById('main-text');
+element.innerHtml='I am suraj';
+var img=document.grtElementById('img');
+img.onClick=function(){
+    img.style.marginLeft='100px';
+}
