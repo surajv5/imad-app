6c7620d4a6717f8025b2f932eb392fcf31e4d7f4
surajv5 <30804526+surajv5@users.noreply.github.com>
2017-08-24 19:53:47 +0530

    Delete mains.js

--- mains.js
+++
@@ -1,7 +0,0 @@
- console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHtml='I am suraj';
-var img=document.getElementById('madi');
-img.onClick=function(){
-    img.style.marginLeft='100px';
-}
