9647595ac890c154e32e705013a2ef2504bacb0a
surajv5 <30804526+surajv5@users.noreply.github.com>
2017-08-24 21:14:36 +0530

    Delete new.js

--- new.js
+++
@@ -1,7 +0,0 @@
-var button=document.getElementById('counter');
-var counter=0;
-button.onClick=function(){
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHtml=counter.toString();
-};
