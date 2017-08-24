1175853ee3c0411f66d8129994e31b0d230ee985
surajv5 <30804526+surajv5@users.noreply.github.com>
2017-08-24 21:08:57 +0530

    Create new.js

---
+++ new.js
@@ -0,0 +1,7 @@
+var button=document.getElementById('counter');
+var counter=0;
+button.onClick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHtml=counter.toString();
+};
