5af55893ccc25dc6b814a5673ab9d113108cb275
surajv5 <30804526+surajv5@users.noreply.github.com>
2017-08-24 20:00:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,7 @@
 console.log('Loaded!');
+alert(
+    "hi i am ur frnd"
+);
 var element=document.getElementById('main-text');
 element.innerHtml='I am suraj';
 var img=document.getElementById('madi');
