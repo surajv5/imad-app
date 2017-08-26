5dc2c6d52d87f86cd8dd3b5743bf04dead99f1e3
surajv5 <30804526+surajv5@users.noreply.github.com>
2017-08-26 16:48:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config={
+    user:'surajv352',
+    database:'surajv352',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.envDB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -73,6 +82,18 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var Pool=newPool(config);
+app.get('/test-db', function (req, res){
+    Pool.query('select * from test', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+            
+        }else{
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 var counter=0;
